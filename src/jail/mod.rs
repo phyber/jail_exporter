a32de6d5a820de98fbeb9afc4ff66bb5991c6ab4
David O'Rourke <david.orourke@gmail.com>
2018-06-05 20:46:07 +0100

    Small tidy up in jail mod

--- src/jail/mod.rs
+++ src/jail/mod.rs
@@ -1,10 +1,19 @@
 extern crate libc;
 
+use libc::{
+    JAIL_DYING,
+    iovec,
+    jail_get,
+};
 use std::ffi::CStr;
 use std::mem::size_of;
 
 // Hardcoded for now to the value of security.jail.param.name on FreeBSD 11.1.
 const JAIL_NAME_LEN: usize = 256;
+
+// jail_get parameter strings
+const PARAM_LASTJID: &[u8] = b"lastjid\0";
+const PARAM_NAME: &[u8] = b"name\0";
 
 // Calls libc::jail_get to get jail jid and name.
 // Contains unsafe code.
@@ -14,19 +23,19 @@ pub fn get(jid: i32) -> (i32, Option<String>) {
 
     // Prepare jail_get parameters.
     let mut iov = vec![
-        libc::iovec{
-            iov_base: b"lastjid\0" as *const _ as *mut _,
-            iov_len:  b"lastjid\0".len(),
+        iovec{
+            iov_base: PARAM_LASTJID as *const _ as *mut _,
+            iov_len:  PARAM_LASTJID.len(),
         },
-        libc::iovec{
+        iovec{
             iov_base: &jid as *const _ as *mut _,
             iov_len:  size_of::<i32>(),
         },
-        libc::iovec{
-            iov_base: b"name\0" as *const _ as *mut _,
-            iov_len:  b"name\0".len(),
+        iovec{
+            iov_base: PARAM_NAME as *const _ as *mut _,
+            iov_len:  PARAM_NAME.len(),
         },
-        libc::iovec{
+        iovec{
             iov_base: value.as_mut_ptr() as *mut _,
             iov_len:  JAIL_NAME_LEN,
         },
@@ -34,10 +43,10 @@ pub fn get(jid: i32) -> (i32, Option<String>) {
 
     // Execute jail_get with the above parameters
     let jid = unsafe {
-        libc::jail_get(
-            iov[..].as_mut_ptr() as *mut libc::iovec,
+        jail_get(
+            iov[..].as_mut_ptr() as *mut iovec,
             iov.len() as u32,
-            libc::JAIL_DYING,
+            JAIL_DYING,
         )
     };
 
