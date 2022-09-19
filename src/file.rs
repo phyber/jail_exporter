92aa582bd41ccad8911f1c46f4cfaaa9b9c2d314
David O'Rourke <david.orourke@gmail.com>
2022-09-19 18:24:22 +0100

    file: Minor improvements to file exporter

--- src/file.rs
+++ src/file.rs
@@ -36,16 +36,7 @@ pub struct FileExporter {
 
 impl FileExporter {
     pub fn new(output: FileExporterOutput) -> Self {
-        match &output {
-            FileExporterOutput::File(path) => {
-                // This was already checked during command line parsing.
-                let path = path.to_str().expect("path to str");
-                debug!("New FileExporter outputting to {}", path);
-            },
-            FileExporterOutput::Stdout => {
-                debug!("New FileExporter outputting to stdout");
-            },
-        };
+        debug!("New FileExporter output to: {}", output);
 
         Self {
             dest: output,
@@ -54,15 +45,13 @@ impl FileExporter {
 
     // Handles choosing the correct output type based on path
     fn write(&self, metrics: Vec<u8>) -> Result<(), ExporterError> {
+        debug!("Writing metrics to: {}", self.dest);
+
         match &self.dest {
             FileExporterOutput::Stdout => {
-                debug!("Writing metrics to stdout");
-
                 io::stdout().write_all(&metrics)?;
             },
             FileExporterOutput::File(path) => {
-                debug!("Writing metrics to {:?}", path);
-
                 // We already vetted the parent in the CLI validator, so unwrap
                 // here should be fine.
                 let parent = path.parent().expect("path to have a parent");
@@ -70,8 +59,7 @@ impl FileExporter {
                 // We do this since we need the temporary file to be on the
                 // same filesystem as the final persisted file.
                 let mut file = NamedTempFile::new_in(&parent)?;
-                let metrics = String::from_utf8(metrics)?;
-                write!(file, "{}", metrics)?;
+                file.write_all(&metrics)?;
                 file.persist(&path)?;
             },
         }
